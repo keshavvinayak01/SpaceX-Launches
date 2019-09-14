1ed889ebeaaaac23c92fea42a8d4a1899093f6e1
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-14 14:16:08 +0530

    Created Key Component

---
+++ client/src/Components/MissionKey.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MissionKey() {
+    return (
+        <div className="my-3">
+            <p>
+                <span className="px-3 mr-2 bg-success" /> = Success
+                <span className="px-3 mr-2 bg-danger" /> = Failed
+            </p>
+        </div>
+    )
+}
+
+export default MissionKey
