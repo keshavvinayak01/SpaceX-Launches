c097b1f9d28bee4044a2eab9be49c7b46a678216
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-14 12:17:49 +0530

    Created React App

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = requre('./schema');
+const schema = require('./schema');
 const axios  = require('axios')
 const app = express();
 
