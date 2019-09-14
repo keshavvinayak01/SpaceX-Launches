9b1dffe91170565b3b255a18ddb3d460f5b1f25a
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-14 16:09:02 +0530

    Logo adjusted

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import { Link } from 'react-router-dom';
 import logo from './spacex.png'
 import Launches from './Components/Launches';
 import SingleLaunch from './Components/SingleLaunch';
@@ -17,10 +18,12 @@ class App extends Component {
 			<ApolloProvider client={client}>
 				<Router>
 					<div className="container">
+					<Link to="/">
 						<img src={logo} 
 						alt="SpaceX" 
-						style={{width:300, display:'block', margin:'auto'}} 
+						style={{height:200, display:'block', margin:'auto'}} 
 						/>
+					</Link>
 						<Route exact path="/" component={Launches} />
 						<Route exact path="/launch/:flight_number" component={SingleLaunch} />
 					</div>
